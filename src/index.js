f40151a9cf4726e3583137aa36b2eebaa9e2a153
Aaron G <aaronhgross@gmail.com>
2018-04-24 16:05:26 -0400

    got rid of TopLevel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TopLevel from './TopLevel';
-// import registerServiceWorker from './registerServiceWorker';
+import Header from './Header';
+import Main from './Main';
+import {createStore} from 'redux';
+import {connect, Provider} from 'react-redux';
+
+let TopLevel = () => 
+  <div>
+    <Header />
+    <Main />
+  </div>
 
 ReactDOM.render(<TopLevel />, document.getElementById('root'));
-// registerServiceWorker();
